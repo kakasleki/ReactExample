726020b3d1f943bda3fba81e0044db8577381a98
kakasleki <kakasleki@naver.com>
2021-08-12 01:25:02 +0900

    Initial commit

---
+++ 2.끝말잇기/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const refreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    name: 'word-relay-setting',
+    mode: 'development', // production,
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app:['./client'],
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 1% in KR', 'last 2 chrome versions'], // browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react'],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ]
+            }
+        }]
+    },
+
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+        new refreshWebpackPlugin(),
+    ],
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/dist/',
+    },
+
+    devServer: {
+        publicPath: '/dist/',
+        hot: true,
+    },
+};
